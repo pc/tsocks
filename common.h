d6818a9e1400d0646e68ef5a6b6123adf81b0dfb
Patrick Collison <patrick@omgfail.local>
2009-04-25 03:44:16 -0700

    Initial commit

---
+++ common.h
@@ -0,0 +1,11 @@
+/* Common functions provided in common.c */
+
+void set_log_options(int, char *, int);
+void show_msg(int level, char *, ...);
+unsigned int resolve_ip(char *, int, int);
+
+#define MSGNONE   -1
+#define MSGERR    0
+#define MSGWARN   1
+#define MSGNOTICE 2
+#define MSGDEBUG  2
